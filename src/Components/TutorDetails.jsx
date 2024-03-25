733cc138bcf62c388499bdee82f161a9df1b1c56
Carlitos <carlitosdutan@pursuit.org>
2024-03-25 16:37:47 -0400

    tutor details started

--- src/Components/TutorDetails.jsx
+++ src/Components/TutorDetails.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const TutorDetails = () => {
+  const API = import.meta.env.VITE_BASE_URL;
+
+  const [selectedTutor, setSelectedTutor] = useState([]);
+
+  const { tutorId } = useParams();
+
+  useEffect(() => {
+    fetch(`${API}/api/users/tutors/${tutorId}`)
+      .then((res) => res.json())
+      .then((data) => setSelectedTutor(data.tutor));
+  });
+
   return (
     <div>
       <h1>Tutor details</h1>
+      <p>{selectedTutor.name}</p>
     </div>
   );
 };
