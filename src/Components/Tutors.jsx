2f584f6f816f32aafa46a7fe40f8fbf4113c066a
Carlitos <carlitosdutan@pursuit.org>
2024-03-25 12:22:41 -0400

    added fetch call

--- src/Components/Tutors.jsx
+++ src/Components/Tutors.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Tutors = () => {
+  const API = import.meta.env.VITE_BASE_URL;
+
+  const [tutors, setTutors] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/api/users`)
+      .then((res) => res.json())
+      .then((data) => setTutors(data));
+  }, [tutors]);
+
   return <div>Tutors</div>;
 };
 
