b42f485bb34e1590b9d09fab4907e7923870bf65
Carlitos <carlitosdutan@pursuit.org>
2024-03-25 14:43:27 -0400

    tutor listing completed

--- src/Components/Tutors.jsx
+++ src/Components/Tutors.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../tutor.css";
 
 const Tutors = () => {
   const API = import.meta.env.VITE_BASE_URL;
@@ -8,10 +9,23 @@ const Tutors = () => {
   useEffect(() => {
     fetch(`${API}/api/users`)
       .then((res) => res.json())
-      .then((data) => setTutors(data));
+      .then((data) => setTutors(data.tutors));
   }, [tutors]);
 
-  return <div>Tutors</div>;
+  return (
+    <div className="tutor-listing">
+      {tutors &&
+        tutors.map(({ profile_pic, name, subject, is_remote, id }) => (
+          <div key={id} className="tutor-card">
+            <img src={profile_pic} alt={profile_pic} />
+            <p>{name}</p>
+            <p>{subject}</p>
+            <p>{is_remote}</p>
+            <button className="view-more">View More...</button>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Tutors;
