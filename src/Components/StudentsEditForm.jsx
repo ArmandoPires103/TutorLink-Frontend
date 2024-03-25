54dbeb3845e1f0967b80e4f2a3f116b2e2962c6b
Carlitos <carlitosdutan@pursuit.org>
2024-03-25 11:35:21 -0400

    base component setup

---
+++ src/Components/StudentsEditForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentsEditForm = () => {
+  return <div>StudentsEditForm</div>;
+};
+
+export default StudentsEditForm;
---
+++ src/Components/StudentsNewForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentsNewForm = () => {
+  return <div>StudentsNewForm</div>;
+};
+
+export default StudentsNewForm;
