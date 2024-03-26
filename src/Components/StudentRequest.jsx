677418c1f14bff13cdb15897ce8f424fd94f1a2b
juligarc91 <julissagarcia@pursuit.org>
2024-03-26 12:42:29 -0400

    renamed components

--- src/Components/StudentRequest.jsx
+++ src/Components/StudentRequest.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// requests a tutor (toggle boolean)
 const StudentRequest = () => {
   return <div>StudentRequest</div>;
 };
--- src/Components/StudentsNewForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StudentsNewForm = () => {
-  return <div>StudentsNewForm</div>;
-};
-
-export default StudentsNewForm;
