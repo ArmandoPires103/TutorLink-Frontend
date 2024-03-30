6ebf73877aacc2311deca5257ec3c5eed5cb2aff
Carlitos <carlitosdutan@pursuit.org>
2024-03-29 21:07:49 -0400

    added nav bar to tutor details plus removed local storage

--- src/Components/TutorDetails.jsx
+++ src/Components/TutorDetails.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import TutorReviews from "./TutorReviews";
 import StudentReviewForm from "./StudentReviewForm";
 import { useOutletContext } from "react-router-dom";
+import Header from "./Header";
 
 const TutorDetails = () => {
   const API = import.meta.env.VITE_BASE_URL;
@@ -28,15 +29,7 @@ const TutorDetails = () => {
     fetch(`${API}/api/users/tutors/${tutorId}`)
       .then((res) => res.json())
       .then((data) => setSelectedTutor(data.tutor))
-      .then(() => {
-        const storedStatus = localStorage.getItem(`bookingStatus_${tutorId}`);
-        console.log(storedStatus);
-        if (storedStatus !== null) {
-          setAvailability(JSON.parse(storedStatus));
-        } else {
-          setAvailability(selectedTutor.is_enrolled);
-        }
-      });
+      .then(() => setAvailability(selectedTutor.is_enrolled));
   }, [tutorId]);
 
   function handleToggleReviews() {
@@ -75,75 +68,75 @@ const TutorDetails = () => {
         }
         return res.json();
       })
-      .then((data) => {
-        console.log("Data inserted!:", data);
-        localStorage.setItem(`bookingStatus_${tutorId}`, true);
-        setAvailability(true);
-      })
+      .then((data) => console.log("Data inserted!:", data))
       .catch((error) => console.error("Error handling booking:", error));
+    setAvailability(true);
   };
 
   const { name, subject, description, profile_pic } = selectedTutor;
 
   return (
-    <div className="tutor-details-wrapper">
-      <div className="tutor-details-content">
-        <h1 className="tutor-card-spacing">{name}</h1>
-        <img
-          src={profile_pic}
-          alt={profile_pic}
-          className="tutor-img tutor-card-spacing"
-        />
-        <p className="tutor-card-spacing">
-          <strong>Expertise: </strong>
-          {subject}
-        </p>
-        <p className="tutor-card-spacing">{description}</p>
-        <div className="button-layout">
-          <Link to="/dashboard" className="view-more button-spacing">
-            <button className="view-more">Back to Home</button>
-          </Link>
-          <button
-            onClick={handleToggleReviews}
-            className="view-more button-spacing"
-          >
-            {!toggleReviews ? "View Reviews" : "Close Reviews"}
-          </button>
-          <button
-            onClick={handleCreateReview}
-            className="view-more button-spacing"
-          >
-            {!toggleCreateReview ? "Create a Review!" : "Cancel"}
-          </button>
-
-          {availability ? (
-            <button className="unavailable-button button-spacing">
-              Tutor Unavailable
-            </button>
-          ) : (
+    <div>
+      <Header />
+      <div className="tutor-details-wrapper">
+        <div className="tutor-details-content">
+          <h1 className="tutor-card-spacing">{name}</h1>
+          <img
+            src={profile_pic}
+            alt={profile_pic}
+            className="tutor-img tutor-card-spacing"
+          />
+          <p className="tutor-card-spacing">
+            <strong>Expertise: </strong>
+            {subject}
+          </p>
+          <p className="tutor-card-spacing">{description}</p>
+          <div className="button-layout">
+            <Link to="/dashboard" className="view-more button-spacing">
+              <button className="view-more">Back to Home</button>
+            </Link>
             <button
+              onClick={handleToggleReviews}
               className="view-more button-spacing"
-              onClick={handleBooking}
             >
-              Book a Session!
+              {!toggleReviews ? "View Reviews" : "Close Reviews"}
             </button>
+            <button
+              onClick={handleCreateReview}
+              className="view-more button-spacing"
+            >
+              {!toggleCreateReview ? "Create a Review!" : "Cancel"}
+            </button>
+
+            {availability ? (
+              <button className="unavailable-button button-spacing">
+                Tutor Unavailable
+              </button>
+            ) : (
+              <button
+                className="view-more button-spacing"
+                onClick={handleBooking}
+              >
+                Book a Session!
+              </button>
+            )}
+          </div>
+
+          {toggleCreateReview && (
+            <StudentReviewForm
+              selectedTutor={selectedTutor}
+              toggleReviews={toggleReviews}
+              setToggleReviews={setToggleReviews}
+            />
+          )}
+          {toggleReviews && (
+            <TutorReviews
+              selectedTutor={selectedTutor}
+              setToggleReviews={setToggleReviews}
+              toggleReviews={toggleReviews}
+            />
           )}
         </div>
-
-        {toggleCreateReview && (
-          <StudentReviewForm
-            selectedTutor={selectedTutor}
-            toggleReviews={toggleReviews}
-            setToggleReviews={setToggleReviews}
-          />
-        )}
-        {toggleReviews && (
-          <TutorReviews
-            selectedTutor={selectedTutor}
-            setToggleReviews={setToggleReviews}
-            toggleReviews={toggleReviews}
-          />
-        )}
       </div>
     </div>
   );
