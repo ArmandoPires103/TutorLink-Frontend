797b9f15ae49710ebb44540f4851f027d2240cb4
Carlitos <carlitosdutan@pursuit.org>
2024-03-29 22:12:21 -0400

    destructured name

--- src/Components/SuccessRequest.jsx
+++ src/Components/SuccessRequest.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const SuccessRequest = ({ selectedTutor }) => {
   const { name } = selectedTutor;
 
+  const [firstName] = name.split(" ");
+
   return (
     <div className="success-request">
       <strong>
         You have successfully requested an appointment with {name}! Please give{" "}
-        {name} some time to view your request.
+        {firstName} some time to view your request.
       </strong>
     </div>
   );
